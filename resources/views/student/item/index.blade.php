c84fd445ca116eacd21da48d983ad79e3bef4ac5
aryndraa <aryacoder1102@gmail.com>
2025-06-06 22:25:51 +0800

    student item

--- resources/views/student/item/index.blade.php
+++ resources/views/student/item/index.blade.php
@@ -1,5 +1,15 @@
 <x-layouts.app :title="__('Loans List')">
     <flux:heading size="xl">Items</flux:heading>
-    <section>
+    <section class="mt-8">
+        <div class="grid grid-cols-4 gap-4 ">
+            <x-student.item-card/>
+            <x-student.item-card/>
+            <x-student.item-card/>
+            <x-student.item-card/>
+            <x-student.item-card/>
+            <x-student.item-card/>
+            <x-student.item-card/>
+            <x-student.item-card/>
+        </div>
     </section>
 </x-layouts.app>
