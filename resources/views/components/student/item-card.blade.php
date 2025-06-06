44cde89a0c650adf73d9c7646e11f7d25be27bdd
aryndraa <aryacoder1102@gmail.com>
2025-06-06 23:47:51 +0800

    loan

--- resources/views/components/student/item-card.blade.php
+++ resources/views/components/student/item-card.blade.php
@@ -1,11 +1,11 @@
-<div class="border border-neutral-600 p-4">
+<div class="bg-neutral-100 p-5 rounded-xl">
     <img
         src="https://images.unsplash.com/photo-1516035069371-29a1b244cc32?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2FtZXJhfGVufDB8fDB8fHww"
         alt=""
-        class="w-full  object-cover aspect-square"
+        class="w-full  object-cover aspect-square rounded-xl"
     >
-    <div class="mt-4 pt-4 border-t border-t-neutral-600">
-        <h2 class="text-xl mb-1 text-neutral-400">Camera</h2>
+    <div class="mt-4 ">
+        <h2 class="text-lg mb-1 text-neutral-400">Camera</h2>
         <p class="text-neutral-500 text-base">Stock : 10</p>
     </div>
 </div>
