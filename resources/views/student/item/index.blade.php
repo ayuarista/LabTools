12d953a5bdc8166c02109fe5f23ecd76a788c5ef
aryndraa <aryacoder1102@gmail.com>
2025-06-07 19:59:54 +0800

    test

--- resources/views/student/item/index.blade.php
+++ resources/views/student/item/index.blade.php
@@ -2,14 +2,23 @@
     <flux:heading size="xl">Items</flux:heading>
     <section class="mt-8">
         <div class="grid grid-cols-4 gap-4 ">
-            <x-student.item-card/>
-            <x-student.item-card/>
-            <x-student.item-card/>
-            <x-student.item-card/>
-            <x-student.item-card/>
-            <x-student.item-card/>
-            <x-student.item-card/>
-            <x-student.item-card/>
+            @forelse($items as $item)
+                <div class="bg-neutral-100 p-5 rounded-xl">
+                    <img
+                            src="{{ $item->image->file_url }}"
+                            alt=""
+                            class="w-full  object-cover aspect-square rounded-xl"
+                    >
+                    <div class="mt-4 ">
+                        <h2 class="text-lg mb-1 text-neutral-400">{{ $item->name }}</h2>
+                        <p class="text-neutral-500 text-base">Stock : {{ $item->quantity }}</p>
+                    </div>
+                </div>
+            @empty
+                <p class="text-center text-gray-500 dark:text-gray-400 p-4">
+                    No items found.
+                </p>
+            @endforelse
         </div>
     </section>
 </x-layouts.app>
